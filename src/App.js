123b61a400d4cecc1b92def46d3d0f0b8bcc7d7f
franquito <franconbiasin@gmail.com>
2018-06-16 13:00:52 -0300

    Install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+const App = () => (
+    <Router>
+      <div>
+        <Route exact path="/" component={Home} />
+        <Route path="/play" component={Play} />
+        <Route path="/history" component={History} />
+        <Route path="/setup" component={Setup} />
       </div>
-    );
-  }
-}
+    </Router>
+);
+
+const Home = () => (
+  <div>
+    <ul>
+      <li>
+        <Link to="/play">Home</Link>
+      </li>
+      <li>
+        <Link to="/history">History</Link>
+      </li>
+      <li>
+        <Link to="/setup">Setup</Link>
+      </li>
+    </ul>
+  </div>
+);
+
+const Play = () => (
+  <div>
+    <h2>Play</h2>
+  </div>
+);
+
+const History = () => (
+  <div>
+    <h2>History</h2>
+  </div>
+);
+
+const Setup = () => (
+  <div>
+    <h2>Setup</h2>
+  </div>
+);
 
 export default App;
