0008ac0bc941b3f86f457ee9349e06a51a07f960
franquito <franconbiasin@gmail.com>
2018-06-20 18:08:13 -0300

    Add PropType to PageTitle

--- src/PageTitle.js
+++ src/PageTitle.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './PageTitle.css';
 
 const PageTitle = ({ children }) => <h1 className="PageTitle">{children}</h1>;
+
+PageTitle.propTypes = {
+  children: PropTypes.node.isRequired
+};
 
 export default PageTitle;
